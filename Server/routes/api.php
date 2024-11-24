b742cd39ee0ba960d84776f6d83286fa79fd42b3
Mr. Tetteh <danielstay73@gmail.com>
2024-11-24 23:54:48 +0000

    added resource

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [User::class, 'register']);
+Route::post('login', [User::class, 'login']);
+
+Route::middleware('auth:sanctum')->group( function () {
+
+});
