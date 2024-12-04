e135752342607a77ef26c0180f0bbd5fa1a86d1b
Mr. Tetteh <danielstay73@gmail.com>
2024-12-04 12:08:40 +0000

    my new changes

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/auth/Login.vue'
 import Register from '@/views/auth/Register.vue'
+import Chat from "@/views/Chat.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: Chat,
     }
    
   ],
