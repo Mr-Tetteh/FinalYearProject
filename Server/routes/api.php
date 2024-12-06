b28ef46711bd369fca80b22cd83d37ce68dcfd68
Mr. Tetteh <danielstay73@gmail.com>
2024-12-06 03:03:04 +0000

    new auth

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [User::class, 'register']);
+Route::post('login', [User::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [User::class, 'logout']);
+    Route::get('user', [User::class, 'user']);
+});
