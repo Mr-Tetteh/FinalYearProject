590131a93447a1be3f2ec1ad8f0204ec62896743
Mr. Tetteh <danielstay73@gmail.com>
2024-12-24 19:28:58 +0000

    patient add to file

--- Server/app/Models/PatientRecord.php
+++ Server/app/Models/PatientRecord.php
@@ -6,7 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class PatientRecord extends Model
 {
-    protected $fillable = ['patient_id', 'user_id', 'symptoms', 'diagnosis', 'treatment', 'temperature', 'pulse_rate', 'sugar_rate', 'admitted', 'ward_number', 'surgery', 'surgery_date', 'surgery_reason', 'additional_notes'];
+    protected $fillable = [
+        'patient_id', 'symptoms', 'diagnosis',
+        'treatment', 'temperature', 'pulse_rate', 'weight', 'sugar_rate', 'admitted',
+        'ward_number', 'surgery', 'surgery_date', 'surgery_reason', 'labs', 'additional_notes',
+        'lab1', 'lab1_results', 'lab2', 'lab2_results', 'lab3', 'lab3_results',
+        'lab4', 'lab4_results', 'lab5', 'lab5_results', 'lab6', 'lab6_results',
+        'lab7', 'lab7_results', 'lab8', 'lab8_results', 'lab9', 'lab9_results',
+        'lab10', 'lab10_results', 'lab11', 'lab11_results', 'lab12', 'lab12_results',
+        'lab13', 'lab13_results', 'lab14', 'lab14_results', 'lab15', 'lab15_results',
+        'drugs_is_paid','labs_is_paid'
+    ];
 
 
 
@@ -14,9 +24,7 @@ class PatientRecord extends Model
         return $this->hasMany(PatientRecord::class, 'patient_id');
     }
 
-    public function user(){
-        return $this->hasMany( Patients::class, 'user_id');
-    }
+
 
 
 
