4fcab945db77c764a70c22c6b60a3264ad010421
Mr. Tetteh <danielstay73@gmail.com>
2024-12-27 01:34:15 +0000

    file upload works

--- Client/src/views/Admin/Patients_info.vue
+++ Client/src/views/Admin/Patients_info.vue
@@ -91,8 +91,9 @@ onMounted(list_patients);
                       </span>
                     <span v-else class="text-muted">None</span>
                   </td>
+
                   <td>
-                      <span v-if="item.medical_history" class="badge bg-warning bg-opacity-10 text-warning">
+                      <span v-if="item.medical_history" class="badge bg-primary bg-opacity-10 text-black ">
                         {{ item.medical_history }}
                       </span>
                     <span v-else class="text-muted">None</span>
