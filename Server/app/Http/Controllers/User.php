3ff76980302c91e8c2c60893f660f2db4b520120
Mr. Tetteh <danielstay73@gmail.com>
2025-01-23 15:16:14 +0000

    password update

--- Server/app/Http/Controllers/User.php
+++ Server/app/Http/Controllers/User.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class User extends Controller
 {
@@ -37,6 +42,70 @@ class User extends Controller
         ]);
 
         return new UserResource($user);
+    }
+
+
+    public function resetPassword(Request $request)
+    {
+        if ($request->input('password') !== $request->input('confirm_password')) {
+            return response()->json(['message' => 'Passwords not match.'], 400);
+        }
+
+        $passwordReset = DB::table('password_resets')
+            ->where('token', $request->input('token'))
+            ->first();
+        logger($request->input('token'));
+        logger($request->all());
+
+        if (!$passwordReset) {
+            return response()->json([
+                'message' => 'Invalid token.'
+            ], 404);
+        }
+
+
+        if (Carbon::parse($passwordReset->created_at)->addHour()->isPast()) {
+            DB::table('password_resets')->where('token', $request->input('token'))->delete();
+
+            return response()->json([
+                'message' => 'Token has expired.'
+            ], 400);
+        }
+
+
+
+        $passwordReset = DB::table('password_resets')->where('token', $request->input('token'))->first();
+
+        $user = \App\Models\User::where('email', $passwordReset->email)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'Sorry User not found'
+            ], 404);
+        }
+
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+        return response()->json([
+            'message' => 'Password reset successfully.'
+        ]);
+    }
+
+    public function forgotPassword(Request $request)
+    {
+        $email = $request->input('email');
+        $token = Str::random(20);
+
+        DB::table('password_resets')->insert([
+            'email' => $email,
+            'token' => $token,
+            'created_at' => Carbon::now()
+        ]);
+        Mail::send('Reset', ['token' => $token], function (Message $message) use ($email) {
+            $message->subject('Reset your Password');
+            $message->to($email);
+        });
+
+        return response()->json(['message' => 'A Reset password link sent on your email. Please check your inbox to update your password.']);
     }
 
     public function all_users()
---
+++ Server/resources/views/Reset.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <title></title>
+    <style>
+        .card {
+            border-radius: 5%;
+        }
+
+        .card-content {
+            padding: 30px;
+            text-align: center;
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+            z-index: -5;
+
+        }
+
+        .button {
+            background-color: deepskyblue;
+            padding: 15px;
+            border-radius: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+<div class="card">
+    <div class="card-content">
+        <div class="inner">
+            <img src="https://drive.google.com/file/d/1kIWYYaGziOdZPD6RU0J2EkF4hqf8w-D1/view" width="30%" alt="Company Logo">
+            <h3 class="text-2xl">We received a request to reset your password.</h3>
+            <p>Use the link below to set up a new password for your account. <br>
+                If you did not request to reset your password, ignore this <br>
+                email and the link will expire on its own. <br>
+                This code will expire in a hour!
+
+            </p>
+            <a href="http://localhost:5174/set_password/{{$token}}"><button class="button">SET NEW PASSWORD HERE</button></a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
